37a7d14c12bc01f2776a6c1dc02e069f43e01dfd
aritify <henrythong93@gmail.com>
2025-05-11 12:27:59 -0700

    Update loop.bash

--- loop.bash
+++ loop.bash
@@ -2,7 +2,7 @@
 
 for n in 4 40 400 4000 40000 400000 1000000 2000000 4000000 8000000
 do
-  g++  -o  simd.proj4  -DARRAYSIZE=$n  simd.proj4.cpp  -lm  -fopenmp
-  #g++  -o  simd.proj4  -DARRAYSIZE=$n  simd.intrinsics.cpp  -lm  -fopenmp
+  g++ -o simd.proj4 -DARRAYSIZE=$n simd.proj4.cpp -lm -fopenmp
+  #g++ -o simd.proj4 -DARRAYSIZE=$n simd.intrinsics.cpp -lm -fopenmp
   ./simd.proj4
 done
